8cfd9f476de596f6806c5b56205e370cdbe362e9
fengluo <fengluoqiuwu@gmail.com>
2024-09-04 17:18:15 +0800

    Initialize

---
+++ include/MyLibrary/algorithms.h
@@ -0,0 +1,12 @@
+//
+// Created by Eden_ on 2024/9/4.
+//
+
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+namespace MyAlgorithms {
+
+}
+
+#endif //ALGORITHMS_H
---
+++ include/MyLibrary/data_structure.h
@@ -0,0 +1,12 @@
+//
+// Created by Eden_ on 2024/9/4.
+//
+
+#ifndef DATA_STRUCTURE_H
+#define DATA_STRUCTURE_H
+
+namespace MyDataStructure {
+
+}
+
+#endif //DATA_STRUCTURE_H
