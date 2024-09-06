5f5efa912050382cefeb647de65ddbc46c56c2be
fengluo <fengluoqiuwu@gmail.com>
2024-09-06 10:55:09 +0800

    Init Google Test

---
+++ lib/data_structure/test/array_test.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Eden_ on 2024/9/5.
+//
+
+#include <gtest/gtest.h>
+#include "DataStructure/data_structure.h"
+
+// 测试类定义
+class ArrayTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST_F(ArrayTest,SimpleConstructorInitTest)
+{
+    // Array a(10);
+    // ASSERT_EQ("[0,0,0,0,0,0,0,0,0,0]",a.to_string());
+}
--- lib/data_structure/test/test.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Eden_ on 2024/9/5.
-//
-
-int main()
-{
-    return 0;
-}
